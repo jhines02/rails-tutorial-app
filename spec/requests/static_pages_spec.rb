8fbbebdf537ed1a3b5e3d33a178dd0d93d986e60
Jeff Hines <jhines02@san.rr.com>
2012-06-21 23:12:25 -0700

    Finished Home and Help pages (Got tests to pass).  [Finished section 3.2.1]

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+    it "should have the content 'Tutorial App'" do
+      visit '/static_pages/home'
+      page.should have_content( 'Tutorial App' )
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content( 'Help' )
+    end
+  end
+
+end
